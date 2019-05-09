baaf2395e412f2dff62fb09320de23c7063af128
2019-SOW-DEV-012 <2019-SOW-DEV-012@gmail.com>
2019-05-09 02:42:22 +0200

    'O' Playes after 'X' and other way around

--- spec/game.spec.js
+++ spec/game.spec.js
@@ -4,8 +4,19 @@ describe("Tic Tac Toe Game", () => {
 
     it("should display current Player as 'X'" , () => {
         game = new TicTacToeGame();
-        
+
         expect(game.currentPlayer).toEqual("X");
     });
 
+
+    it("should 'O' Playes after 'X' and other way around" , () => {
+        game = new TicTacToeGame();
+        let playedPosition = 1;
+        game.recordPlayerMoveOnBoard(playedPosition);
+
+        expect(game.currentPlayer).toEqual("O");
+
+    });
+
+    
 }); 
--- src/game.js
+++ src/game.js
@@ -1,4 +1,11 @@
 function game(){
-    this.currentPlayer = "X"
+
+    this.currentPlayer = "X";
+
+    this.recordPlayerMoveOnBoard = function(currentPlayedPosition) {
+        this.currentPlayer = this.currentPlayer === "X" ? "O":"X";
+    };
+
+    
 }
 module.exports = game; 
