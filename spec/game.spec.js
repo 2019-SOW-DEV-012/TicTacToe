6d323f5d38398c7f6bd8f6d7d19698b8e87afc06
2019-SOW-DEV-012 <2019-SOW-DEV-012@gmail.com>
2019-05-09 02:34:29 +0200

    Initial project set-up

---
+++ spec/game.spec.js
@@ -0,0 +1,6 @@
+describe("Tic Tac Toe Game", () => {
+    it("should test jasmine is OK?" , () => {
+        expect(true).toEqual(true);
+    });
+
+}); 
---
+++ webpack.config.js
@@ -0,0 +1,15 @@
+var path = require('path');
+
+module.exports = {
+devServer: {
+	    contentBase: path.join(__dirname, './src'),
+	    compress: true,
+	    port: 8080
+},
+  entry: {
+    game: './src/main.js'
+},
+output: {
+    filename: 'game.js'
+},
+};
