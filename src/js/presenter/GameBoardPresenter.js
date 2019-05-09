559ca047219b121af0cf25d86191a829538db850
2019-SOW-DEV-012 <2019-SOW-DEV-012@gmail.com>
2019-05-10 00:14:25 +0200

    Extract add played tile to board

--- src/js/presenter/GameBoardPresenter.js
+++ src/js/presenter/GameBoardPresenter.js
@@ -7,17 +7,24 @@ function GamePresenter(playerInfo,view) {
     if(this.tiles[playedPosition]){
       return
     }
-    this.tiles[playedPosition] = playerInfo.currentPlayer;
+
+    addPlayedTileToBoard(playedPosition,this.tiles);
     let topRowWin = [0, 1, 2];
     if(this.tiles[topRowWin[0]]=== this.tiles[topRowWin[1]] && this.tiles[topRowWin[0]] === this.tiles[topRowWin[2]]){
       this.winner = this.tiles[topRowWin[0]];
       view.displayGameStatus(this.winner);
     }
-    view.displayOwnerOnTile(playerInfo.currentPlayer,playedPosition);
+
     playerInfo.currentPlayer = switchPlayer()
-    view.displayPlayerTurn(playerInfo.currentPlayer)
   };
 
+  function addPlayedTileToBoard(playedPosition,tiles){
+    tiles[playedPosition] = playerInfo.currentPlayer;
+    view.displayOwnerOnTile(playerInfo.currentPlayer,playedPosition);
+    view.displayPlayerTurn(playerInfo.currentPlayer)
+
+
+  }
   function switchPlayer() {
     return playerInfo.currentPlayer === "X" ? "O" : "X";
   }
